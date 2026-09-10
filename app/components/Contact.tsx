1722d5ab59c970c15973448783cfa2ec303a9fc8
Prathap S <prathapece1234@gmail.comexitgit config --global user.name prathapece1234git config --global user.email prathapece1234@gmail.com>
2026-09-10 22:44:47 +0530

    updated page

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -84,12 +84,18 @@ export default function Contact() {
               rel="noopener noreferrer"
               className="social-link"
               id="social-linkedin"
-              title="LinkedIn"
+              title="Connect with me on LinkedIn"
             >
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z" />
                 <circle cx="4" cy="4" r="2" />
               </svg>
+              <span>Let's Connect on LinkedIn</span>
             </a>
           </div>
         </div>
